fd7f50a73bbe7e70bf4e12e3e1bdd9abe331046c
Robert French <178658682+rfrench3@users.noreply.github.com>
2025-08-17 18:40:16 -0400

    update template elements to the project

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 """
-PySide6 Flatpak Template
-
 Created in 2025 by rfrench3 (TealMango) - https://github.com/rfrench3
 
 Licensed under the GNU GPLv3 only. See LICENSE file in the project root for full license information.
@@ -14,7 +12,7 @@ import sys
 import os
 
 # locating other application files
-sys.path.insert(0, "/app/share/pyside6apptemplate") # flatpak path
+sys.path.insert(0, "/app/share/ublue-updater") # flatpak path
 from program_file_locator import DATA_DIR
 from widget_manager import app_icon, load_widget, load_message_box
 
@@ -22,21 +20,21 @@ from widget_manager import app_icon, load_widget, load_message_box
 from PySide6.QtWidgets import QApplication, QPushButton #Import widgets here as needed
 
 # Edit the .ui file using Qt Designer
-ui_main = os.path.join(DATA_DIR, "main_window.ui")
+ui_main = os.path.join(DATA_DIR, "main.ui")
 
-# logic for the main windowscopebuddy-guiscopebuddy-gui
+# logic for the main window
 
 class MainWindow():
     def __init__(self, window): 
         self.window = window
 
         # connect ui elements to code
-        self.button = self.window.findChild(QPushButton,"pushButton")
+        self.update = self.window.findChild(QPushButton,"update")
 
         # Connect actions to slots or functions
-        self.button.clicked.connect(self.button_clicked)
+        self.update.clicked.connect(self.activate_update)
         
-    def button_clicked(self):
+    def activate_update(self):
         load_message_box(
             self.window,
             "message box title!",
